364296e0b478d98c930b315e791fed5be19276bf
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-08-03 09:48:25 +0200

    created basic component for the AllPlaces screen

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,7 +1,33 @@
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import PlaceItem from "./PlaceItem";
 
-function PlacesList({places}) {
-return <FlatList data={places} keyExtractor={(item) => item.id} renderItem={}/>
+function PlacesList({ places }) {
+    if (!places || places.length === 0) {
+        return <View style={styles.fallBackContainer}>
+    <Text style={styles.fallBackText}>No places added yet - start adding some!</Text>
+        </View>
+    }
+
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => {
+        <PlaceItem place={item} />;
+      }}
+    />
+  );
 }
 
 export default PlacesList;
+
+const styles = StyleSheetSheet.create({
+    fallBackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    },
+    fallBackText: {
+        fontSize: 16,
+    },
+});
--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,6 +1,7 @@
+import PlacesList from "../components/places/PlacesList";
 
 function AllPlaces() {
-
+return <PlacesList />
 }
 
 export default AllPlaces;
