0849e931d110e6cfdd5cc3cb56fcb56383f6fbfe
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-08-03 16:15:25 +0200

    transfered the data so the lat and long shows in console.log on pickedlocation

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -7,7 +7,7 @@ import {
   PermissionStatus,
 } from "expo-location";
 import { useEffect, useState } from "react";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 import {
   useNavigation,
   useRoute,
@@ -35,7 +35,15 @@ function LocationPicker({ onPickLocation }) {
   }, [route, isFocused]);
 
   useEffect(() => {
-    onPickLocation(pickedLocation);
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = await getAddress(
+          pickedLocation.lat,
+          pickedLocation.lng
+        );
+        onPickLocation({...pickedLocation, address: address});
+      }
+    }
   }, [pickedLocation, onPickLocation]);
 
   async function verifyPermissions() {
