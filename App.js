eefc6f433bded2934e494e65b23c232cf4107e2c
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-08-04 10:26:37 +0200

    added the placedetails screen and binded everything now waiting for data when on details screen

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import Map from "./screens/Map";
 import { useEffect, useState } from "react";
 import { init } from "./util/database";
 import AppLoading from "expo-app-loading";
+import PlaceDetails from "./screens/PlaceDetails";
 
 const Stack = createNativeStackNavigator();
 
@@ -62,6 +63,7 @@ export default function App() {
             }}
           />
           <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen name="placeDetails" component={PlaceDetails}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
